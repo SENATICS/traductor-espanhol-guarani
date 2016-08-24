98becd6faf79e6dd13f448568f71859ac1965a9f
Andres Ortiz <ortizvillalba@gmail.com>
2016-08-24 08:57:26 -0400

    se corrige redirección

--- traductor/index.php
+++ traductor/index.php
@@ -11,27 +11,10 @@
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/bootbox.js"></script>        
+        <meta http-equiv="refresh" content="0; url=traducir.php" />
+
     </head>
 
 <a href="traducir.php">Traductor iframe</a>
-<br><br>
-
-<a href="traducir-full.php">Traductor Full</a>
-    
-
-
-<script>
-
-    $.ajax({
-        //url: "http://localhost/guarani/ws.php"
-        url: "http://localhost/guarani/ws.php?method=buscar&palabra=abajo&traducira=gu"
-    }).then(function(data) {
-        
-        var palabra2 = data.palabra2;
-        alert("content es: *"+palabra2+"*");
-
-    });
-
-</script>
 
 </html>
